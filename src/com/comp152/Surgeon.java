3fe693284090c4a7f5e5b6ec86c79f75868717f1
bgravanda <bgravanda@student.bridgew.edu>
2021-09-30 11:46:16 -0400

    Surgeon

---
+++ src/com/comp152/Surgeon.java
@@ -0,0 +1,18 @@
+package com.comp152;
+
+import java.util.Random;
+
+public class Surgeon extends Doctor{
+    private String hospital;
+    public Surgeon(String hospital){
+        super(999.99, "Fancy Name Brand School");
+        this.hospital = hospital;
+    }
+    public void treatPatient(Person patient){
+        var hoursPicker = new Random();
+        System.out.println("After "+hoursPicker.nextInt(8)+" hours of surgery at "+hospital+ "our patient should be on the mend");
+        patient.getTreated();
+
+    }
+
+}
